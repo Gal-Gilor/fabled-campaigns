8b576ee029e00bce36f5c0111e8ba19b1074daa5
Gal Gilor <gilorgal2010@gmail.com>
2026-06-06 13:30:55 -0400

    feat(wiki): add Monster, MagicItem, and FilterConfig types

---
+++ types/wiki.ts
@@ -0,0 +1,44 @@
+export type MagicItemRarity =
+  | 'Common'
+  | 'Uncommon'
+  | 'Rare'
+  | 'Very Rare'
+  | 'Legendary'
+  | 'Artifact';
+
+export type MagicItem = {
+  slug: string;
+  name: string;
+  itemType: string;
+  rarity: MagicItemRarity;
+  requiresAttunement: boolean;
+  attunementBy?: string;
+  body: string;
+};
+
+export type Monster = {
+  slug: string;
+  name: string;
+  size: string;
+  type: string;
+  alignment: string;
+  ac: string;
+  hp: string;
+  speed: string;
+  str: number;
+  dex: number;
+  con: number;
+  int: number;
+  wis: number;
+  cha: number;
+  cr: string;
+  body: string;
+};
+
+export type FilterConfig<T> = {
+  key: string;
+  label: string;
+  type: 'search' | 'select';
+  getValue: (item: T) => string;
+  options?: string[];
+};
